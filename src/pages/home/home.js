3178d931f613f881087383fa6c999c8da769fc8a
David Dominguez <david@Davids-MacBook-Pro.local>
2022-04-21 15:18:52 -0500

    first template

---
+++ src/pages/home/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <p>Has to redirect</p>
+  </div>
+)
+
+export default Home
---
+++ src/pages/signin/signin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './signin.css';
+import logo from '../../assets/images/avianca-logo.png';
+
+const SignIn = () => (
+  <div className="container">
+    <img src={logo} className="avianca-logo" alt="logo" />
+    <p>Signin</p>
+  </div>
+)
+
+export default SignIn
